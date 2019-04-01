7e5bb72bf60fc9e2051d52a46a654157860fec10
Unknown <dracolytch@yahoo.com>
2019-04-01 17:32:39 -0400

    Extended transform, and added range class

---
+++ Assets/EditorExtensions/Examples/Scripts/ExampleLookScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleLookScript : MonoBehaviour
+{
+    public GameObject viewTarget;
+    public int t2;
+    public RangeColor myColor5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Look away from the target, handy for quads
+        //transform.LookAwayFrom(viewTarget);
+
+        // Where should I look to look at something?
+        // You wouldn't do this, but it's an example of getitng the look rotation
+        //transform.rotation = transform.GetLookAtRotation(viewTarget);
+
+        // Get the rotation to look away from the target
+        // You wouldn't do this, but it's an example of getitng the method
+        transform.rotation = transform.GetLookAwayFromRotation(viewTarget);
+    }
+}
---
+++ Assets/EditorExtensions/Extensions/TransformExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformExtensions
+{
+    public static Quaternion GetLookAtRotation(this Transform self, Vector3 target)
+    {
+        return Quaternion.LookRotation(target - self.position);
+    }
+
+    public static Quaternion GetLookAtRotation(this Transform self, Transform target)
+    {
+        return GetLookAtRotation(self, target.position);
+    }
+
+    public static Quaternion GetLookAtRotation(this Transform self, GameObject target)
+    {
+        return GetLookAtRotation(self, target.transform.position);
+    }
+
+
+
+    public static void LookAwayFrom(this Transform self, Vector3 target)
+    {
+        self.rotation = GetLookAwayFromRotation(self, target);
+    }
+
+    public static void LookAwayFrom(this Transform self, Transform target)
+    {
+        self.rotation = GetLookAwayFromRotation(self, target);
+    }
+
+    public static void LookAwayFrom(this Transform self, GameObject target)
+    {
+        self.rotation = GetLookAwayFromRotation(self, target);
+    }
+
+
+
+    public static Quaternion GetLookAwayFromRotation(this Transform self, Vector3 target)
+    {
+        return Quaternion.LookRotation(self.position - target);
+    }
+
+    public static Quaternion GetLookAwayFromRotation(this Transform self, Transform target)
+    {
+        return GetLookAwayFromRotation(self, target.position);
+    }
+
+    public static Quaternion GetLookAwayFromRotation(this Transform self, GameObject target)
+    {
+        return GetLookAwayFromRotation(self, target.transform.position);
+    }
+}
